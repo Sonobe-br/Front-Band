2bbef87ff5f325b907b0a74d2e889da72ec0d266
Daniel-TI <daniel.silveira@indbandeirante.com.br>
2022-11-04 09:44:17 -0300

    ajustes js

--- lab1.js
+++ lab1.js
@@ -1,4 +1,4 @@
-//Front-End
+//Front-End : Sessão onde o usuário faz o preenchimento dos dados 
 let carga1 = 50;
 let carga2 = 275;
 let carga3 = 9950;
@@ -7,7 +7,7 @@ let cargaMaxima = 5000;
 let cargaMinima = 1000;
 
 let frota1 = 'oito eixos';
-let frota2 = 'doze eixos';
+let frota2 = 'doze eixos';-
 
 //Back-End
 let cargaSomada = carga1 + carga2 + carga3;
@@ -37,4 +37,3 @@ if (cargaResultante >= cargaMaxima) {
 const produtosIndustriais = [ 'Pote Verde', 'Pote Amarelo', 'Pote Azul', 'Pote Branco', 'Pote Rosa', 'Pote Cinza', 'Pote Roxo', 'Pote Grande', 'POte Médio'];
 
 console.log(produtosIndustriais.map(produtoIndustrial => produtoIndustrial.length));
- 

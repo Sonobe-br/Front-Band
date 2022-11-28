d366f94c33283965052b719d78e427eba1bffc2f
Daniel-TI <daniel.silveira@indbandeirante.com.br>
2022-11-28 15:59:28 -0300

    labs para iniciantes

---
+++ lab3.js
@@ -0,0 +1,25 @@
+let a = "dani";
+let b = "sonobe";
+let c = "silveira";
+
+let concatenar = a + b + c;
+
+const nomeCompleto = () => {
+
+    let formatacaoDeNome = concatenar;
+    console.log (formatacaoDeNome);
+
+}
+nomeCompleto ();
+
+if (concatenar === "") {
+
+    console.log("servidor rodando!")
+    
+    
+} else {
+    
+    console.log("servidor parado!")
+
+}
+
